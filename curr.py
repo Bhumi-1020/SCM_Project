bb240e0a6fe2600a1b7bc4cca9b99dc61b5d5ec1
Bhavika-2004 <122029247+Bhavika-2004@users.noreply.github.com>
2023-03-24 10:54:19 +0530

    Update curr.py
    added ui

--- curr.py
+++ curr.py
@@ -21,3 +21,19 @@ def convert(self, from_currency, to_currency, amount):
 url = 'https://api.exchangerate-api.com/v4/latest/USD'
 converter = RealTimeCurrencyConverter(url)
 print(converter.convert('INR','USD',100))
+
+def __init__(self, converter):
+    tk.Tk.__init__(self)
+    self.title = 'Currency Converter'
+    self.currency_converter = converter
+    
+    self.geometry("500x200")
+
+#Label
+self.intro_label = Label(self, text = 'Welcome to Real Time Currency Convertor',  fg = 'blue', relief = tk.RAISED, borderwidth = 3)
+self.intro_label.config(font = ('Courier',15,'bold'))
+
+self.date_label = Label(self, text = f"1 Indian Rupee equals = {self.currency_converter.convert('INR','USD',1)} USD \n Date : {self.currency_converter.data['date']}", relief = tk.GROOVE, borderwidth = 5)
+
+self.intro_label.place(x = 10 , y = 5)
+self.date_label.place(x = 170, y= 50)
